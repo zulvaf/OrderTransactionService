520893b7a5d4bb7f56e15272897d20cf3244fcc8
Zulva Fachrina <zfachrina@gmail.com>
2017-11-08 09:57:22 +0700

    Change config for heroku

--- app/dbconnection.js
+++ app/dbconnection.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL || '',
+  user: process.env.PGUSER || '',
+  host: process.env.PGHOST || '',
+  database: process.env.PGDATABASE || '',
+  password: process.env.PGPASSWORD || '',
+  port: process.env.PGPORT || ''
 });
 
 module.exports = pool;
